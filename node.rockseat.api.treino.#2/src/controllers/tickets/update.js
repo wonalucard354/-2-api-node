b192806ebd5ed7b43ee67ff65d195c621503f2b6
Lucas Nunes Dumas <128542655+wonalucard354@users.noreply.github.com>
2024-11-07 13:43:29 -0300

    api de ticket
    sua primeira api completa

---
+++ node.rockseat.api.treino.#2/src/controllers/tickets/update.js
@@ -0,0 +1,13 @@
+export function update({ request, response, database }) {
+
+    const { id } = request.params
+    const { equipment, description } = request.body
+
+database.update("tickets", id, {
+    equipment,
+    description,
+    updated_at: new Date(),
+})
+
+    return response.end()
+}
---
+++ node.rockseat.api.treino.#2/src/routes/index.js
@@ -0,0 +1,8 @@
+import { tickets } from "./tickets.js";
+import { parseRoutePath } from "../utils/parseRoutePath.js";
+
+export const routes = [...tickets].map((route) => ({
+    ...route,
+    path: parseRoutePath(route.path),
+})
+)
